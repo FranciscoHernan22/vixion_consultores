eacc02cca9e9b4ed22874038d77da702c24e29a3
francisco hernan <Franciscogjjgjgjh@gmail.com>
2026-03-08 19:30:12 -0700

    primer commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,ts,tsx}"
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        vixion: {
+          dark: "#101425",
+          primary: "#21375b",
+          soft: "#97accb",
+          light: "#cbd6ea",
+        },
+      },
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
