85517f739c680d6f9e91c15609805ef9e80cbe8c
francisco hernan <Franciscogjjgjgjh@gmail.com>
2026-03-14 17:36:29 -0700

    primer commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,25 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
+
+        animation: {
+        ceo: "pulseCEO 3s infinite"
+      },
+
+      keyframes: {
+        pulseCEO: {
+          "0%": {
+            boxShadow: "0 0 0px rgba(59,130,246,0.6)"
+          },
+          "50%": {
+            boxShadow: "0 0 25px rgba(59,130,246,0.9)"
+          },
+          "100%": {
+            boxShadow: "0 0 0px rgba(59,130,246,0.6)"
+          }
+           }
+        },
+
       colors: {
         vixion: {
           dark: "#101425",
